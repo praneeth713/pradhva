b42068f0cf4c7772e1a815c9674f9a2a751b7845
Nani <lakshminarayanamalineni1@gmail.com>
2023-10-07 15:13:16 +0530

    version change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import logo from './components/logo.jpg';
 import './App.css';
-
 import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import Item from './components/Item';
@@ -8,25 +8,20 @@ import LoadingBar from 'react-top-loading-bar'
 import data from "./components/data.json"
 import Home from "./components/Home";
 
-const App = ()=> {
-  const [progress, setProgress] = useState(0)
- 
-    return ( 
-      <div> 
+
+function App() {
+  // const [progress, setProgress] = useState(0)
+  return (
+    <div> 
         <Router>
         <NavBar/> 
-        <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={progress} />
         <Switch>
           <Route exact path="/"><Home/></Route>
-          <Route exact path="/Enroll"><Item setProgress={setProgress}  key="Home" parsedData={data} country="US" category="Home"/></Route>
+          <Route exact path="/Enroll"><Item  key="Home" parsedData={data} country="US" category="Home"/></Route>
         </Switch>
         </Router>
       </div>
-    )
- 
+  );
 }
 
 export default App;
