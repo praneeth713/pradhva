d5e8e68c608d0b1ddd304f7d9c324380896c6de8
Nani <lakshminarayanamalineni1@gmail.com>
2023-10-08 14:56:36 +0530

    CSS changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-import "./Home.css"
+import "./Home.css";
 const Home = () => {
   return (
     <div>
-        <div className="img"></div>
-        <div className="center">
-            <div className="title">Welcome to PRADHVA</div>
-            <div className="sub_title">IIITK Intra sports meet 23-24</div>
-            <div className="btns">
-                <button><Link className="nav-link" to="/Enroll">Explore</Link></button>
-            </div>
+      <div className="img"></div>
+      <div className="centr">
+        <div className="title">Welcome to PRADHVA</div>
+        <div className="sub_title">IIITK Intra sports meet 23-24</div>
+        <div className="btns">
+          <button>
+            <Link className="nav-link" to="/Enroll">
+              Explore
+            </Link>
+          </button>
         </div>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
